6890122060dc71d08758f165d88dc17c2fe6d4ba
Karthik3708 <karthiksooriya007@gmail.com>
2025-05-28 21:56:04 -0700

    3rd puzzle included

--- Utilities/Puzzles.py
+++ Utilities/Puzzles.py
@@ -7,9 +7,36 @@ words = ['apple', 'banana', 'cat', 'dog', 'elephant', 'fish',
          'whale', 'xylophone', 'yacht', 'zebra']
 
 hints = ['no of corners of circle', 'beggining', 'the only even prime','trianle','the only number spelled with same no of letters as its value',
-           'no of rings in olympics','sides of cube','thala for a reason','no of planets in solar system','atomic number of fluorine',]
+           'no of rings in olympics flag','sides of cube','thala for a reason','no of planets in solar system','atomic number of fluorine',]
 
-digit = random.randint(100,999)
+print("-----------------------------------------------------------------------------")
+
+def hint_converter():
+
+    print("Find the number.")
+    print("Here are your hints for the digits:")
+
+    global hints
+
+    digit = random.randint(100, 1000000)
+
+    digilist = [int(d) for d in str(digit)]
+
+    for i in range(0,len(digilist)):
+        x = digilist[i]
+        print(str(i+1)+'. '+hints[x])
+
+    ans = int(input("Enter the digit: "))
+
+    if ans == digit:
+        print("Correct!")
+        print("-----------------------------------------------------------------------------")
+    else:   
+        print("Incorrect! The correct answer was:", digit)
+        print("-----------------------------------------------------------------------------")
+
+
+
 
 def jumble_words():
     
@@ -26,12 +53,18 @@ def jumble_words():
     
    if ans == word:
       print("Correct!")
+      print("-----------------------------------------------------------------------------")
+
    else:
       print("Incorrect! the correct answer was:", word)
+      print("-----------------------------------------------------------------------------")
+
+
+
 
 def find_the_word(): 
     result = []
-    no = random.randint(1,3)
+    no = random.randint(1,2)
     randomword= random.choice(words)
     for char in randomword:         
         next_char = chr(ord(char) + no) 
@@ -43,11 +76,17 @@ def find_the_word():
 
     if answer==randomword:
         print("Correct!!!")
+        print("-----------------------------------------------------------------------------")
+
     else:
         print("Incorrect! The correct answer was:", randomword)
+        print("-----------------------------------------------------------------------------")
+
 
 
 
 if __name__ == "__main__":
+
+    hint_converter()
     jumble_words()
     find_the_word()
